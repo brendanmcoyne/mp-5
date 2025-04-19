0c3d904a8662591803a8660bb7dd0822f9640bde
brendanmcoyne <bmcoyne@bu.edu>
2025-04-18 23:27:41 -0400

    try now

--- app/db.ts
+++ app/db.ts
@@ -12,12 +12,17 @@ let client: MongoClient | null = null;
 let db: Db | null = null;
 
 async function connect(): Promise<Db> {
-        if (!client) {
-            client = new MongoClient(MONGO_URI);
+    if (!client) {
+        client = new MongoClient(MONGO_URI, {serverSelectionTimeoutMS: 10000,});
+        try {
             await client.connect();
+        } catch (error) {
+            console.error("Error connecting to MongoDB:", error);
+            throw new Error("Failed to connect to MongoDB");
         }
-        return client.db(DB_NAME);
     }
+    return client.db(DB_NAME);
+}
 
 export default async function getCollection(collectionName: string): Promise<Collection> {
     if (!db) {
